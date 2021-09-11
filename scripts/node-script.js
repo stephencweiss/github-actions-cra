deeec36d0710e217be1d9af1e47c9ed981482a1f
Stephen <stephen.c.weiss@gmail.com>
2021-09-11 14:01:40 -0500

    ✨ Building the node script

--- scripts/node-script.js
+++ scripts/node-script.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
 console.log('I am a simple script!')
+
+// read the package.json
+// parse the version
+// update the version
+// save the package.json
+
+const fs = require('fs');
+
+const rawPackage = fs.readFileSync('../package.json')
+const parsedPackage = JSON.parse(rawPackage)
+console.log({parsedPackage})
