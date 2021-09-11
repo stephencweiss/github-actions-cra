03db1c59a668a55b7ef7b9d7d3c0c7d9c2e198ce
Stephen <stephen.c.weiss@gmail.com>
2021-09-11 14:43:13 -0500

    ✨ Working Script

--- scripts/node-script.js
+++ scripts/node-script.js
@@ -1,14 +1,32 @@
 #!/usr/bin/env node
-
-console.log('I am a simple script!')
-
-// read the package.json
-// parse the version
-// update the version
-// save the package.json
-
 const fs = require('fs');
 
-const rawPackage = fs.readFileSync('./package.json')
-const parsedPackage = JSON.parse(rawPackage)
-console.log({parsedPackage})
+function incrementMinorVersion(){
+
+    function _incMinorVersion(version){
+        const [major, minor, patch] = version.split('.');
+        const newMinor = String(Number(minor) +1);
+        return [major, newMinor, patch].join('.')
+    }
+
+    // read the package.json
+    const rawPackage = fs.readFileSync('./package.json')
+
+    // parse the version
+    const parsedPackage = JSON.parse(rawPackage)
+    const currentVersion = parsedPackage?.version
+
+    // update the version
+    const nextVersion = _incMinorVersion(currentVersion)
+    parsedPackage.version = nextVersion
+
+    // save the package.json
+    fs.writeFile('./package.json', Buffer.from(JSON.stringify(parsedPackage)), {encoding: 'utf8' }, (err) => {
+        if (err) {
+            return console.log('Error!', err)
+        }
+        return console.log('All clear')
+    })
+}
+
+incrementMinorVersion();
